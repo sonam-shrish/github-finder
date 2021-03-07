185228054903dc32b8e96a51e174d6ab02b30cb2
sonam-shrish <sonamshrish1618@gmail.com>
2021-03-07 19:58:24 +0545

    A little bit of routing
    Routing About to /about

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//the <Route /> syntax is kinda sloppy. So, check that out
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -5,6 +6,9 @@ import axios from 'axios'
 import {Component} from 'react';
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import {Fragment} from 'react'
+import About from './components/pages/About'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 class App extends Component {
@@ -37,8 +41,10 @@ class App extends Component {
 
   //set Alert
   setAlert = (msg, type) => {
-    this.setState({alert:{msg, type}})
-
+    this.setState({alert:{msg, type}})//send the message and type of alert
+    setTimeout(()=> this.setState({
+      alert:null
+    }), 4000)
   }
   // componentDidMount() {
   //   //In order not to exhaust the request we are equipping the request url with the client id and client secret which is stored in .env.local
@@ -53,20 +59,33 @@ class App extends Component {
   render () {
 
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className='container'>
       <Alert alert={this.state.alert} />
-      <Search 
-      searchUsers = {this.searchUsers} 
-      clearUsers={this.clearUsers} 
-      setAlert={this.setAlert}
-      showClear= {this.state.users.length!==0?true:false}/>
-      <div className='container'>
-      <Users loading={this.state.loading} users={this.state.users}/>
-      </div>
+      
+      <Switch>
+        <Route exact path='/' render={props => (
+          <Fragment>
+          <Search 
+          searchUsers = {this.searchUsers} 
+          clearUsers={this.clearUsers} 
+          setAlert={this.setAlert}
+          showClear= {this.state.users.length!==0?true:false}/>
+          <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users}/>
+          </div>
+          </Fragment>
+        )} />
+        <Route exact path='/about' component={About} />
+      </Switch>
+
+
+      
       </div>
     </div>
+    </Router>
   );
 }
 
