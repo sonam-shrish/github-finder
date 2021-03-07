d9ff55de2360d806c4a4efef92cf3f5065221395
sonam-shrish <sonamshrish1618@gmail.com>
2021-03-07 17:29:56 +0545

    Search by fetching from github
    making a search request and displaying them in

--- src/App.js
+++ src/App.js
@@ -9,16 +9,28 @@ import Search from './components/users/Search'
 class App extends Component {
   state= {
     users:[],
-    loading: true
+    loading: false
   }
-  componentDidMount() {
-    //In order not to exhaust the request we are equipping the request url with the client id and client secret which is stored in .env.local
-    axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res=>{
+  //search users
+  searchUsers = (text)=>
+  {
+    this.setState({
+      loading:true
+    })
+    axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res=>{
   this.setState({
-    users:res.data,
+    users:res.data.items,
     loading:false
-  })} )//the res.data is an array of objects
+  })})
   }
+  // componentDidMount() {
+  //   //In order not to exhaust the request we are equipping the request url with the client id and client secret which is stored in .env.local
+  //   axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res=>{
+  // this.setState({
+  //   users:res.data,
+  //   loading:false
+  // })} )//the res.data is an array of objects
+  // }
   
   
   render () {
@@ -26,7 +38,7 @@ class App extends Component {
   return (
     <div className="App">
       <Navbar/>
-      <Search />
+      <Search searchUsers = {this.searchUsers}/>
       <div className='container'>
       <Users loading={this.state.loading} users={this.state.users}/>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,16 +4,19 @@ class Search extends Component {
     state = {
         text:''
     };
-    onChange = (e) => {
+    
+    onChange = e =>
         this.setState({text: e.target.value})
-
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text)
     }
     render() {
         return (
-            <div className='form'>
+            <div className='form' onSubmit={this.onSubmit}>
                 <form>
                     <input type='text' name='Search' placeholder='Search Users...' value={this.state.text} onChange={this.onChange}></input>
-                    <input value='Search' type='submit' className='btn btn-dark btn-block' ></input>
+                    <input value='Search' type='submit'   className='btn btn-dark btn-block' ></input>
 
                 </form>
                 
